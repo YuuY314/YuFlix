ed5128053e767e996b0f78fa979e7a119a708b4d
YuuY314 <rafaelyu0202@gmail.com>
2026-01-04 14:29:18 -0300

    Add Project (Lessons 1 to 3)

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <h1>Hello World!</h1>
+      <p>Lorem Ipsum</p>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <h2>YuFlix &copy; Desenvolvido por Rafael Yu 2026</h2>
+        </footer>
+    );
+}
+
+export default Footer;
