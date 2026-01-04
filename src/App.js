653675d25d97a3a6882eea8cabba3b324d22a72f
YuuY314 <rafaelyu0202@gmail.com>
2026-01-04 18:32:12 -0300

    Lesson 6

--- src/App.js
+++ src/App.js
@@ -3,8 +3,19 @@ import Footer from "./components/Footer";
 import Banner from "./components/Banner";
 import Container from "./components/Container";
 import Card from "./components/Card";
+import Category from "./components/Category";
 
-import videos from "./json/db.json";
+import videos from "./json/videos.json";
+
+const categories = [
+  "Fantasia",
+  "Herói",
+  "Animação"
+];
+
+function filterCategory(id) {
+  return videos.filter(video => video.category === categories[id])
+}
 
 function App() {
   return (
@@ -12,10 +23,15 @@ function App() {
       <Header/>
       <Banner image="favorites"/>
       <Container>
-        <h2>Fantasia</h2>
-        <section className="cards">
-          {videos.map((video) => <Card id={video.id} key={video.id}/>)}
-        </section>
+        <Category category="Fantasia">
+          {filterCategory(0).map((video) => <Card id={video.id} key={video.id}/>)}
+        </Category>
+        <Category category="Herói">
+          {filterCategory(1).map((video) => <Card id={video.id} key={video.id}/>)}
+        </Category>
+        <Category category="Animação">
+          {filterCategory(2).map((video) => <Card id={video.id} key={video.id}/>)}
+        </Category>
       </Container> 
       <Footer/>
     </>
