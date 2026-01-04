031e878e8e81565871a5baf479aa0341c2948ebf
YuuY314 <rafaelyu0202@gmail.com>
2026-01-04 18:54:03 -0300

    Lesson 7

--- src/App.js
+++ src/App.js
@@ -3,35 +3,21 @@ import Footer from "./components/Footer";
 import Banner from "./components/Banner";
 import Container from "./components/Container";
 import Card from "./components/Card";
-import Category from "./components/Category";
-
-import videos from "./json/videos.json";
-
-const categories = [
-  "Fantasia",
-  "Herói",
-  "Animação"
-];
-
-function filterCategory(id) {
-  return videos.filter(video => video.category === categories[id])
-}
+import Category, {categories, filterCategory} from "./components/Category";
 
 function App() {
   return (
     <>
       <Header/>
-      <Banner image="favorites"/>
+      <Banner image="home"/>
       <Container>
-        <Category category="Fantasia">
-          {filterCategory(0).map((video) => <Card id={video.id} key={video.id}/>)}
-        </Category>
-        <Category category="Herói">
-          {filterCategory(1).map((video) => <Card id={video.id} key={video.id}/>)}
-        </Category>
-        <Category category="Animação">
-          {filterCategory(2).map((video) => <Card id={video.id} key={video.id}/>)}
-        </Category>
+        {
+          categories.map((category, index) => 
+            <Category category={category}>
+              {filterCategory(index).map((video) => <Card id={video.id} key={video.id}/>)}
+            </Category>
+          )
+        }
       </Container> 
       <Footer/>
     </>
--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,4 +1,16 @@
+import videos from "../../json/videos.json";
+
 import styles from "./Category.module.css";
+
+export const categories = [
+  "Fantasia",
+  "Herói",
+  "Animação"
+];
+
+export function filterCategory(id) {
+  return videos.filter(video => video.category === categories[id])
+}
 
 function Category({category, children}) {
     return (
