b7094d418023f90aec35f82f15ac3694a7200f8e
YuuY314 <rafaelyu0202@gmail.com>
2026-01-08 16:37:26 -0300

    Lesson 18

---
+++ src/pages/VideoRegister/index.js
@@ -0,0 +1,18 @@
+import Header from "../../components/Header";
+import Container from "../../components/Container";
+import Footer from "../../components/Footer";
+import Form from "../../components/Form";
+
+function VideoRegister() {
+    return (
+        <>
+            <Header/>
+            <Container>
+                <Form/>
+            </Container>
+            <Footer/>
+        </>
+    );
+}
+
+export default VideoRegister;
