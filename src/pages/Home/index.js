5c7e7f16c3841555bacb0286f11f42feb6a36849
YuuY314 <rafaelyu0202@gmail.com>
2026-01-08 13:24:07 -0300

    Lesson 12

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,28 +4,13 @@ import Banner from "../../components/Banner";
 import Container from "../../components/Container";
 import Card from "../../components/Card";
 import Category, {categories, filterCategory} from "../../components/Category";
-import {useState} from "react";
 
 function Home() {
-  function getName(e) {
-    console.log(e.target.value);
-    setName(e.target.value);
-  }
-
-  const [name, setName] = useState();
-
   return (
     <>
       <Header/>
       <Banner image="home"/>
       <Container>
-        <input
-          type="text"
-          placeholder="Nome"
-          onChange={getName}
-        />
-        <h2>{name}</h2>
-
         {
           categories.map((category, index) => 
             <Category category={category}>
